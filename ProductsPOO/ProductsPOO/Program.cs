ec6442b2b405cc3d54563af7b9ca7e8c620c23a7
David Perez <perezdavid2236@gmail.com>
2022-02-27 16:18:35 -0500

    date Class

---
+++ ProductsPOO/ProductsPOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
